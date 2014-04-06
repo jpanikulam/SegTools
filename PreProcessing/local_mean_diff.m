15df673a22b962abfbb9eb8a8f99ee15101d34b0
jpanikulam <jpanikul@gmail.com>
2014-04-06 03:23:05 -0400

    Add local mean difference

---
+++ PreProcessing/local_mean_diff.m
@@ -0,0 +1,24 @@
+
+
+function difference = local_mean_diff(image, hsize)
+%% Local Mean Difference
+% Finds the dissimilarity between a pixel and its neighboring pixels
+% To do this, I take the difference between each pixel and the average of
+% all of the pixels in a radius of hsize around it
+
+%coeff = ones(hsize);
+%coeff = coeff./numel(coeff);
+%avg_value_padded = conv2(double(image),coeff);
+%avg_value = avg_value_padded(1:size(image,1),1:size(image,2));
+%avg_value = avg_value_padded(end-size(image,1)+1:end,end-size(image,2)+1:end);
+
+%% Real Function.
+%Turns out pillbox method is VASTLY superior to square convolution. This
+%is also because I didn't accurately compensate for the "phase shift" (lol)
+%of the image when conv2 is applied.
+avg_filter = fspecial('disk',hsize(1));
+avg_value = imfilter(image,avg_filter);
+
+difference = double(avg_value)-double(image);
+figure,imagesc(double(avg_value)-double(image));
+
--- Transform/morletRotate.m
+++ Transform/morletRotate.m
@@ -1,9 +1,10 @@
 function [ imgr,imgi, imgf ] = morletRotate( P , sc)
+%% Do not use this function - it will cause a crash
 %morletRotate - Rotates a morlet wavelet at intervals of 10deg and takes
 %the maxima
 %   nothing too funky fresh
 
-cwtmorl = cwtft2(P,'wavelet',{'morl',{6,1,1}},'scales',sc,'angles',0:deg2rad(10):pi-deg2rad(10));
+%cwtmorl = cwtft2(P,'wavelet',{'morl',{6,1,1}},'scales',sc,'angles',0:deg2rad(10):pi-deg2rad(10));
 %cwtmorl = cwtft2(P,'wavelet',{'esmexh', {1,0.5}},'scales',5,'angles',0:deg2rad(10):pi-deg2rad(10));
 imgi = max(imag(cwtmorl.cfs),[],5);
 
